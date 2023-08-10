f21b526b6586d56def58a6c17b863d40d7dab50a
Evan Yang <87679953+evanyang22@users.noreply.github.com>
2023-08-10 17:03:28 -0400

    Add files via upload

--- excerptAutoAligner.py
+++ excerptAutoAligner.py
@@ -1,7 +1,7 @@
 import os
-inputDir=r"./input"
+inputDir=r"./DirectoryContainingMyInputSample"
 for file in os.scandir(inputDir):
     print(file.name)
-    command="sudo docker run -v ~/input:/exceRptInput -v ~/output:/exceRptOutput -v ~/DATABASE/hg38:/exceRpt_DB/hg38 -t rkitchen/excerpt INPUT_FILE_PATH=/input/" + str(file.name)
+    command="sudo docker run -v ~/DirectoryContainingMyInputSample:/DirectoryContainingMyInputSample -v ~/DirectoryInWhichToPutMyResults:/DirectoryInWhichToPutMyResults -v ~/DATABASE/hg38:/exceRpt_DB/hg38 -t rkitchen/excerpt INPUT_FILE_PATH=/DirectoryContainingMyInputSample/" + str(file.name)
     print(command)
     os.system(command)
